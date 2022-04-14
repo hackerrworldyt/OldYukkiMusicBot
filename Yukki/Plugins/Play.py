45eae973207110d61eae9a64bb71d6d36bfca95e
hackerrworldyt <103627199+hackerrworldyt@users.noreply.github.com>
2022-04-14 12:53:21 +0530

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -33,7 +33,7 @@ from Yukki.Utilities.youtube import (get_yt_info_id, get_yt_info_query,
 from Yukki.Utilities.func import mplay_stream, vplay_stream
 
 @app.on_message(
-    commandpro(["/p", "Play", "/play", "/play@{BOT_USERNAME}"]) & filters.group
+    filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
