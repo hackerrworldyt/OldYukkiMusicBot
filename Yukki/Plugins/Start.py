744b80279a8c0c9f8fba0855c6e13dbfd07ad2d5
hackerrworldyt <103627199+hackerrworldyt@users.noreply.github.com>
2022-04-14 13:45:13 +0530

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -145,6 +145,23 @@ async def useradd(_, message: Message):
             ]
         ),
     )
+    
+@app.on_message(filters.command(["alive", "alive"]) & filters.private & ~filters.edited)
+async def useradd(_, message: Message):
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Siesta Robot.** \n\n"
+  TEXT += "💠 **I'm Working Properly** \n\n"
+  TEXT += f"💠 **My Master : [Vain](https://t.me/saint_foire)** \n\n"
+  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
+  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/Siestaxbot?start=help"), Button.url("Support", "https://t.me/machinaxsupport")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+                ]
+            ]
+        ),
+    )
+    
 
 
 @app.on_message(filters.command(["repo"]) & filters.group & ~filters.edited)
